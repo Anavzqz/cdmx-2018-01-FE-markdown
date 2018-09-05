05eea74045ae559feef1cfa48d09a909d1bdf9a2
Ana Vzqz <ana.maria.vl@hotmail.com>
2018-09-05 18:26:35 -0500

    Intentando agregar path

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 //File System 
 const fs = require('fs');
+const fetch = require('node-fetch');
+const file = './README.md';
+
+//path
+const route = (file) => {
+    return file = resolve(file);
+};
+route('./README.md');
 
 // Funcion encargada de leer el documento .md
-const readFile = () => {
-    fs.readFile('./README.md', 'utf-8',  (err, data) => {
+const readFile = (callback) => {
+    fs.readFile(file, 'utf-8',  (err, data) => {
         if (err) {
             console.log('Tienes un error, verifica'); 
         }else {
@@ -23,14 +31,42 @@ const url = (data) => {
     const expReg = /(http:\/\/|https:\/\/|www\.)[^\s\)]+/gim;
     const getUrl = data.match(expReg);
         console.log(getUrl);
-    //array.forEach(element => {
-        
-   // });
-    
-
-
 };
 
+const newArray = (getName, getUrl) => {
+    let infoObj = [];
+    let i = 0;
+    for (i; i < getUrl.length; i++){
+        infoObj.push({
+        href: getUrl[i],
+        text: getName[i],
+        file: route(file)
+    });
+}
+newArray();
+}
+
+/*const validate = () => {
+    let i = 0;
+    for (i; i < getUrl.length; i++) {
+        fetch(getUrl[i])
+        .then((res) => {
+            console.log(res);
+            //Si pasa el status, damos una condición
+            if(status === 200){
+                return 
+            }
+        });
+    }
+}*/
+
+//md links(path, options)
+//path (ruta absoluta o relativa al archivo o directorio).
+
+//options: Un objeto con las siguientes propiedades:
+
+//validate: Valor que determina si se desea validar los links encontrados en el archivo. (tipo de dato booleano)
+//stats: Valor que determina si se desea calcular los stats de de los links encontrados en el archivo. (tipo de dato booleano)
 
 
 
