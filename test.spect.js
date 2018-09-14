9a4eadc96b84bda1164b61f17a96a44901825bdc
Ana Vzqz <ana.maria.vl@hotmail.com>
2018-09-14 12:55:39 -0500

    Agregando archivo con promesas, aun le falta modificaciones

---
+++ test.spect.js
@@ -0,0 +1,8 @@
+//let util = require('./mdLinks.js').default; // Trae el archivo que se debe testear
+
+//describe('readFile function', () =>{
+  //  it ('readFile ',()=>{
+    //    expect(true).toBe(true);
+  
+//    })
+//});
